8c449f5f83d1d31cfb8117eae336f579fb8ba6ed
Shivam Kumar <143876488+Shivam-Kumar-Chauhan@users.noreply.github.com>
2023-09-12 11:01:13 +0000

    JS objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,39 @@
+// singleton
+// object.create
+
+// object litrals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Shivam",
+    "full name": "Shivam Chauhan",
+    [mySym]: "mykey1",
+    age: "22",
+    location: "Dehli",
+    email: "shivamkumar@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "shivamkumar@chatgpt.com";
+// Object.freeze(JsUser);
+JsUser.email = "shivamkumar@netflix.com";
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
