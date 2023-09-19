3f565fe1d7bd9d4e8a52b9f80274b7527443ed42
Shivam Kumar <143876488+Shivam-Kumar-Chauhan@users.noreply.github.com>
2023-09-19 11:43:10 +0000

    This and Arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "shivam",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} ,  welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username =  "shiv"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username =  "shivam"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function(){
+// let username = "shivam"
+// console.log(this.username);
+// }
+
+
+const chai = () => {
+    let username = "shivam"
+    console.log(this);
+    }
+    
+    // chai()
+
+    // const addTwo = (num1, num2) => {
+    //     return num1 + num2
+    // }
+
+    // const addTwo = (num1, num2) => num1 + num2
+    // const addTwo = (num1, num2) => (num1 + num2)
+
+    const addTwo = (num1, num2) => ({username: "shivam"})
+
+
+    console.log(addTwo(5, 3));
